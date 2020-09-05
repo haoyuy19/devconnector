ec3e857e48997da86c7501fb36a3171763a1270b
Haoyu Yang <haoyuy19@gmail.com>
2020-09-04 22:16:53 -0700

    Added profile

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -21,8 +21,15 @@ export default function (state = initialState, action) {
     case PROFILE_ERROR:
       return {
         ...state,
-        profile: payload,
-        loading: false
+        profile: null,
+        loading: false,
+        error: payload
+      };
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: []
       };
     default:
       return state;

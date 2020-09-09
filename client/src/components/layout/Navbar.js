333417dfb0706480e5b43b75d94fd17f38c9d575
Haoyu Yang <haoyuy19@gmail.com>
2020-09-08 17:38:44 -0700

    Profile display added

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className='navbar bg-dark'>
       <h1>
         <Link to='/'>
-          <i className='fas fa-code'></i> DevConnector
+          <i class='fab fa-connectdevelop'></i> DeveloperSpace
         </Link>
       </h1>
       {!loading && (
